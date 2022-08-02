574465d06fb59431e7fb96beec033df9ce277f52
Maruf <maruf@gmail.com>
2022-08-02 12:10:39 +0600

    add all

--- js/main.js
+++ js/main.js
@@ -1,70 +1,71 @@
-var preloader = $('#preloader');
-$(function () {
+// var preloader = $('#preloader');
+// $(function () {
 
-    var wind = $(window);
-    toggleNav();
-
-
-    const navbar = document.querySelector('#main-nav');
-
-    // initialize scrollspy
-    const scrollspy = new VanillaScrollspy(navbar);
-    scrollspy.init();
-
-    // hide pre loading after document loaded
-    preloader.addClass('hide');
+//     var wind = $(window);
+//     toggleNav();
 
 
+//     const navbar = document.querySelector('#main-nav');
 
+//     // initialize scrollspy
+//     const scrollspy = new VanillaScrollspy(navbar);
+//     scrollspy.init();
 
-    // control navbar
-    $(window).on("scroll", () => {
-        toggleNav();
-    })
-
-
-    //features top items
-    var featureTopItem = $('.features__top-content .item');
-    $('.features__top-content .item .item-icon').click((e) => {
-        featureTopItem.removeClass('active');
-        const selectedItem = e.currentTarget.parentElement;
-        $(selectedItem).addClass('active');
-    })
+//     // hide pre loading after document loaded
+//     preloader.addClass('hide');
 
 
 
-    // faq accordion
-    var faqAccordions = $('.faq__accordion');
-    var accHeading = $(".faq__accordion-question");
+
+//     // control navbar
+//     $(window).on("scroll", () => {
+//         toggleNav();
+//     })
+
+
+//     //features top items
+//     var featureTopItem = $('.features__top-content .item');
+//     $('.features__top-content .item .item-icon').click((e) => {
+//         featureTopItem.removeClass('active');
+//         const selectedItem = e.currentTarget.parentElement;
+//         $(selectedItem).addClass('active');
+//     })
+
+
+
+//     // faq accordion
+//     var faqAccordions = $('.faq__accordion');
+//     var accHeading = $(".faq__accordion-question");
     
 
-    accHeading.click((e) => {
-        faqAccordions.removeClass('active');
-        const selectedItem = e.currentTarget.parentElement;
-        $(selectedItem).addClass('active');
-    })
+//     accHeading.click((e) => {
+//         faqAccordions.removeClass('active');
+//         const selectedItem = e.currentTarget.parentElement;
+//         $(selectedItem).addClass('active');
+//     })
 
 
     
 
 
-    function toggleNav() {
-        const navbar = $(".navbar")
-        const scrollY = wind.scrollTop();
-        // console.log(scrollY);
+//     function toggleNav() {
+//         const navbar = $(".navbar")
+//         const scrollY = wind.scrollTop();
+//         // console.log(scrollY);
 
-        if (scrollY > 100) {
-            navbar.addClass('navbar-show');
-        } else {
-            navbar.removeClass('navbar-show');
-        }
-    }
-})
-
-
+//         if (scrollY > 100) {
+//             navbar.addClass('navbar-show');
+//         } else {
+//             navbar.removeClass('navbar-show');
+//         }
+//     }
+// })
 
 
 
-$(window).on("load", () => {
-    //window load function
-})
+  document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '#image-carousel', {
+        type: 'loop'
+    } );
+    splide.mount();
+  } );
